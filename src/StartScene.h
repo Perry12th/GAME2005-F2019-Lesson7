4446398eae64897603e8513a3d80dfa0853d8c5c
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2019-10-18 16:23:06 -0400

    Add project files.

---
+++ src/StartScene.h
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef __START_SCENE__
+#define __START_SCENE__
+
+#include "Scene.h"
+#include "Label.h"
+#include "ship.h"
+
+class StartScene : public Scene
+{
+public:
+	StartScene();
+	~StartScene();
+
+	// Inherited via Scene
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	virtual void handleEvents() override;
+	virtual void start() override;
+
+private:
+	/*Label* m_pStartLabel;
+	Label* m_pInstructionsLabel;*/
+
+	Ship* m_pShip;
+
+	glm::vec2 m_mousePosition;
+
+	// ImGui utility functions
+	void m_ImGuiKeyMap();
+	void m_ImGuiSetStyle();
+	void m_updateUI();
+
+	// ImGui menu variables
+	bool m_exitApp = false;
+	bool m_displayAbout = false;
+	bool m_displayUI = true;
+
+	// Phyiscs Variables
+	bool m_isGravityEnabled = false;
+	float m_gravity = 9.8f;
+	glm::vec2 m_position = glm::vec2(0.0f, 0.0f);
+	glm::vec2 m_velocity = glm::vec2(0.0f, 0.0f);
+	glm::vec2 m_acceleration = glm::vec2(0.0f, 0.0f);
+	float m_PPM = 30; // pixels per meter
+	float m_time = 0.01666f; // time step
+	float m_Atime = 0.01666f; // accel time
+
+	// Physics Functions
+	void m_move();
+};
+
+#endif /* defined (__START_SCENE__) */
